250793c0832045afb4ba042073215c172e2d9aec
Thomas Malt <thomas@malt.no>
2013-02-14 00:46:25 +0100

    added gruntfile stub

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: tm
+ * Date: 2/14/13
+ * Time: 12:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+};
