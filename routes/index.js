75e3c200eeeb9c03961a7a0d8237bbaa4c163af4
Thomas Malt <thomas@malt.no>
2013-02-26 00:32:26 +0100

    added stub handler for fb post

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
-
+var util = require('util');
 
 exports.index = function(req, res) {
     // counter += 1;
     // var data = {counter: counter};
-    res.render('index', {});
+    var data = {
+        'FACEBOOK_APP_ID': process.env.FACEBOOK_APP_ID,
+        'FACEBOOK_SECRET': process.env.FACEBOOK_SECRET
+    };
+    
+    console.log("GOT CALL TO INDEX:", util.inspect(data));
+    res.render('index', data);
 };
 
 exports.admin = function(req, res) {
     res.send("<h1>Admin</h1>");
-}
+};
+
+exports.fblogin = function(req, res) {
+    console.log("got fblogin post: ", util.inspect(req.body));  
+};
