aa822243640904a36cde123e98e4fe866bb090df
Thomas Malt <thomas@malt.no>
2013-02-27 00:18:43 +0100

    started implementing the db and leaerning mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,31 @@
 var util = require('util');
+var mg = require('mongoose');
+
+console.log ('mongodb: ', process.env.MONGOLAB_URI);
+mg.connect(process.env.MONGOLAB_URI);
+var db = mg.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Callback got called.");
+});
+
+var userSchema = mg.Schema({
+    first_name: String,
+    last_name:  String,
+    nick_name: String,
+    facebook: {
+        id:     Number,
+        link:   String,
+        name:   String,
+        gender: String,
+        username: String,
+        languages: Array 
+    },
+    created_at: Date,
+    updated_at: Date
+});
+
+var User = mg.model('User', userSchema);
 
 exports.index = function(req, res) {
     // counter += 1;
@@ -9,6 +36,8 @@ exports.index = function(req, res) {
     };
     
     console.log("GOT CALL TO INDEX:", util.inspect(data));
+    console.log("req.session: ", util.inspect(req.session));
+    console.log("req.cookies: ", util.inspect(req.cookies));
     res.render('index', data);
 };
 
@@ -17,5 +46,19 @@ exports.admin = function(req, res) {
 };
 
 exports.fblogin = function(req, res) {
-    console.log("got fblogin post: ", util.inspect(req.body));  
+    console.log("got fblogin post: ");
+    var data = req.body;
+    var user = new User({
+        first_name:     data.first_name,
+        last_name:      data.last_name,
+        facebook: data,
+        created_at: new Date(),
+        updated_at: new Date()
+    });
+    user.save();
+    console.log("saved");
+};
+
+exports.fb_loginstatus = function(req, res) {
+    console.log("got fb_loginstatus: ", req.body);
 };
